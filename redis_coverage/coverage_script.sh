196c2668b565ad4c5218463d5d36f17fc01441f1
Vinod Gupta <vinod@rapidfort.com>
2023-01-09 11:01:55 -0800

    fix coverage script

--- redis_coverage/coverage_script.sh
+++ redis_coverage/coverage_script.sh
@@ -13,8 +13,9 @@ echo "Running image generation for $0 $1 $2"
 
 IREGISTRY=docker.io
 IREPO=bitnami/redis
-OREPO=rapidfort/redis-rfstub
-PUB_REPO=rapidfort/redis
+OACCOUNT=codervinod
+OREPO=${OACCOUNT}/redis-rfstub
+PUB_REPO=${OACCOUNT}/redis
 HELM_RELEASE=redis-release
 
 
@@ -39,7 +40,8 @@ test_no_tls()
     local IMAGE_REPOSITORY=$1
     echo "Testing redis without TLS"
     # Install redis
-    helm install ${HELM_RELEASE}  ${IREPO} --namespace ${NAMESPACE} --set image.tag=${TAG} --set image.repository=${IMAGE_REPOSITORY} -f overrides.yml
+    helm install ${HELM_RELEASE}  ${IREPO} --namespace ${NAMESPACE} \
+        --set image.tag=${TAG} --set image.repository=${IMAGE_REPOSITORY} -f overrides.yml
 
     # sleep for 3 min
     echo "waiting for 3 min for setup"
@@ -83,7 +85,11 @@ test_tls()
     sleep 1m
 
     # Install redis
-    helm install ${HELM_RELEASE} ${IREPO} --namespace ${NAMESPACE} --set image.tag=${TAG} --set image.repository=${IMAGE_REPOSITORY} --set tls.enabled=true --set tls.existingSecret=localhost-server-tls --set tls.certCAFilename=ca.crt --set tls.certFilename=tls.crt --set tls.certKeyFilename=tls.key -f overrides.yml
+    helm install ${HELM_RELEASE} ${IREPO} --namespace ${NAMESPACE} \
+        --set image.tag=${TAG} --set image.repository=${IMAGE_REPOSITORY} \
+        --set tls.enabled=true --set tls.existingSecret=localhost-server-tls \
+        --set tls.certCAFilename=ca.crt --set tls.certFilename=tls.crt \
+        --set tls.certKeyFilename=tls.key -f overrides.yml
 
     # sleep for 3 min
     echo "waiting for 3 min for setup"
@@ -93,7 +99,8 @@ test_tls()
     REDIS_PASSWORD=$(kubectl get secret --namespace ${NAMESPACE} ${HELM_RELEASE} -o jsonpath="{.data.redis-password}" | base64 --decode)
 
     #exec into container
-    kubectl -n ${NAMESPACE} exec -it ${HELM_RELEASE}-master-0 -- /bin/bash -c "REDISCLI_AUTH=\"${REDIS_PASSWORD}\" redis-cli -h localhost --tls --cert /opt/bitnami/redis/certs/tls.crt --key /opt/bitnami/redis/certs/tls.key --cacert /opt/bitnami/redis/certs/ca.crt EVAL \"return {KEYS[1],KEYS[2],ARGV[1],ARGV[2]}\" 2 key1 key2 first second"
+    kubectl -n ${NAMESPACE} exec -it ${HELM_RELEASE}-master-0 \
+        -- /bin/bash -c "REDISCLI_AUTH=\"${REDIS_PASSWORD}\" redis-cli -h localhost --tls --cert /opt/bitnami/redis/certs/tls.crt --key /opt/bitnami/redis/certs/tls.key --cacert /opt/bitnami/redis/certs/ca.crt EVAL \"return {KEYS[1],KEYS[2],ARGV[1],ARGV[2]}\" 2 key1 key2 first second"
 
     # sleep for 30 sec
     echo "waiting for 30 sec"
@@ -127,7 +134,7 @@ harden_image()
     # Push stub to our dockerhub account
     docker push ${PUB_REPO}:${TAG}
 
-    echo "Hardened images pushed to ${PUB_REPO}:${TAG}" 
+    echo "Hardened images pushed to ${PUB_REPO}:${TAG}"
 }
 
 
